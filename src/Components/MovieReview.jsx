5e731ee34d9fd1a876d17b1a521d975de9834d9e
FREEDOM <freewhynane62@gmail.com>
2021-08-04 13:36:50 +0000

    add component to handle api fetching

---
+++ src/Components/MovieReview.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MovieReview extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Awesome Movies to Watch</h1>
+            </div>
+        );
+    }
+}
+
+export default MovieReview;
