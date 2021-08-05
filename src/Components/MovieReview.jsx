3bb71e70bf40749347c0fb4300bc0f4059f4491a
FREEDOM <freewhynane62@gmail.com>
2021-08-05 00:36:32 +0000

    add axios in place of fetch

--- src/Components/MovieReview.jsx
+++ src/Components/MovieReview.jsx
@@ -1,32 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import MovieList from "./MovieList";
+import axios from "axios";
+import { Container, Row, Col } from "react-bootstrap";
 
 class MovieReview extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            movies: [],
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+    };
+  }
 
-    componentDidMount(){
-        fetch("https://api.nytimes.com/svc/movies/v2/reviews/all.json?offset=20&api-key=1c4Xl5uOwljOzjIMKiBbmfIbRsB1NkZD").then((res) => res.json()).then((movies) => { this.setState({
-            movies: movies
-        });
-        console.log(movies)
-    }).catch((err) => {
-        console.log(err)
-    });
-    }
+  componentDidMount() {
+    axios
+      .get(
+        "https://api.nytimes.com/svc/movies/v2/reviews/all.json?offset=20&api-key=1c4Xl5uOwljOzjIMKiBbmfIbRsB1NkZD"
+      )
+      .then((res) => {
+        this.setState({ movies: res.data.results });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    
-
-    render() {
-        return (
-            <div>
-                <h1>Awesome Movies to Watch</h1>
-            </div>
-        );
-    }
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col sm="12" md="12" lg="12">
+              <h1>Awesome Movies to Watch</h1>{" "}
+            </Col>
+            <Col>
+              <MovieList movies={this.state.movies} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default MovieReview;
